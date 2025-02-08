36edc8f8d1898e4c59c1307182a286b34c6a49c0
Lucas Martins Vasconcelos <lmartins2205@gmail.com>
2025-02-07 22:09:56 -0300

    fix error message

--- src/controllers/productController/index.js
+++ src/controllers/productController/index.js
@@ -60,11 +60,11 @@ const deleteProduct = (req, res) => {
     res.status(200).json(result);
   } catch (error) {
     if (error.message === "Product not found") {
-      return res.status(404).json({ message });
+      return res.status(404).json({ message: error.message });
     }
 
     if (error.message === "Cannot delete an available product") {
-      return res.status(400).json({ message });
+      return res.status(400).json({ message: error.message });
     }
 
     return res.status(500).json({ message: "An unexpected error occurred." });
